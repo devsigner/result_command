7d8954a35bf8e7157d440a4ad14af6f23d7eccee
cedric.d <cedricdarricau@gmail.com>
2021-04-12 11:19:35 +0200

    fix spec_helper require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ end
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'simple_result_command'
+require 'result_command'
 
 Dir[File.join(File.dirname(__FILE__), 'factories', '**/*.rb')].each do |factory|
   require factory
